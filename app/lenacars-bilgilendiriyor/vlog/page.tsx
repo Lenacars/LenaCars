d1eca531e65bdc976886159b0507a3015d2b48cb
Lenacars <info@lenacars.com>
2025-04-05 17:47:53 +0300

    Video sayfası için oluşturuldu

---
+++ app/lenacars-bilgilendiriyor/vlog/page.tsx
@@ -0,0 +1,8 @@
+export default function Vlog() {
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold">Vlog</h1>
+      <p>Video içeriklerimize bu sayfadan ulaşabilirsiniz.</p>
+    </div>
+  );
+}
