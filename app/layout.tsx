04f7c822e33e8b44bd1cc7f47219366960f65426
Lenacars <info@lenacars.com>
2025-04-06 00:32:53 +0300

    header içerisinde bileşenlerine ayırma

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import Header from "@/components/ui/site-header"
+import SiteHeader from "@/components/layout/site-header"
 import Footer from "@/components/footer"
 
 const inter = Inter({ subsets: ["latin"] })
