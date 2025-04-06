2410c3501a9ad130c764a7a4759bf55191e7ccae
Lenacars <info@lenacars.com>
2025-04-06 19:18:10 +0300

    Update site-header.tsx

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -2,14 +2,14 @@
 
 import TopBar from "@/components/layout/TopBar"
 import MainHeader from "@/components/layout/MainHeader"
-import NavigationMenuComponent from "@/components/layout/NavigationMenu"
+import NavigationMenuDesktop from "@/components/layout/NavigationMenuDesktop"
 
 export default function SiteHeader() {
   return (
     <header className="w-full">
       <TopBar />
       <MainHeader />
-      <NavigationMenuComponent />
+      <NavigationMenuDesktop />
     </header>
   )
 }
