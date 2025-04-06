187531b291b138f4601f433d3376c04a6f285532
Lenacars <info@lenacars.com>
2025-04-06 18:41:00 +0300

    Update TopBar.tsx

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -6,11 +6,9 @@ import TopBarMobile from "./TopBarMobile"
 export default function TopBar() {
   return (
     <div className="w-full">
-      {/* Masaüstü için */}
       <div className="hidden md:block">
         <TopBarDesktop />
       </div>
-      {/* Mobil için */}
       <div className="block md:hidden">
         <TopBarMobile />
       </div>
