b8d69ce32e0d05715dc38a4dfb9a9674de1334ae
Lenacars <info@lenacars.com>
2025-04-06 17:51:31 +0300

    Update site-header.tsx

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -1,19 +1,34 @@
 import TopBarDesktop from "@/components/layout/TopBarDesktop"
 import TopBarMobile from "@/components/layout/TopBarMobile"
-import MainHeader from "@/components/layout/MainHeader"
-import NavigationMenu from "@/components/layout/NavigationMenu"
+import MainHeaderDesktop from "@/components/layout/MainHeaderDesktop"
+import MainHeaderMobile from "@/components/layout/MainHeaderMobile"
+import NavigationMenuDesktop from "@/components/layout/NavigationMenuDesktop"
 import NavigationMenuMobile from "@/components/layout/NavigationMenuMobile"
 
 export default function SiteHeader() {
   return (
     <header className="w-full">
-      <TopBar />
-      <MainHeader />
-      {/* Navigation Menüler */}
+      {/* TopBar */}
       <div className="hidden md:block">
-        <NavigationMenu />
+        <TopBarDesktop />
       </div>
-      <div className="md:hidden flex justify-end px-4 py-2">
+      <div className="block md:hidden">
+        <TopBarMobile />
+      </div>
+
+      {/* MainHeader */}
+      <div className="hidden md:block">
+        <MainHeaderDesktop />
+      </div>
+      <div className="block md:hidden">
+        <MainHeaderMobile />
+      </div>
+
+      {/* NavigationMenu */}
+      <div className="hidden md:block">
+        <NavigationMenuDesktop />
+      </div>
+      <div className="block md:hidden">
         <NavigationMenuMobile />
       </div>
     </header>
