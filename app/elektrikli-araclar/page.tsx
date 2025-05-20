1682834fa2b65486bcc2e80b6ffc925579421a89
Lenacars <info@lenacars.com>
2025-05-20 13:45:10 +0300

    Delete app/elektrikli-araclar directory

--- app/elektrikli-araclar/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ElektrikliAraclar() {
-  return (
-    <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold">Elektrikli Araçlar</h1>
-      <p>Elektrikli araç seçenekleri, avantajları ve güncel modeller hakkında bilgiler burada yer alacaktır.</p>
-    </div>
-  );
-}
