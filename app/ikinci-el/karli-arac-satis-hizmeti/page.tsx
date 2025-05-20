31b513554a5be4a27f6cfccd97708fbfd75ea5e4
Lenacars <info@lenacars.com>
2025-05-20 13:44:51 +0300

    Delete app/ikinci-el directory

--- app/ikinci-el/karli-arac-satis-hizmeti/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function KarliAracSatisHizmeti() {
-  return (
-    <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold">Karlı Araç Satış Hizmeti</h1>
-      <p>Araç sahipleri için avantajlı satış modeli hakkında bilgiler burada yer alacak.</p>
-    </div>
-  );
-}
