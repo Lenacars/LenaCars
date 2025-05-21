712380828cf316a3a4dfab30517af4c9b4353c65
Lenacars <info@lenacars.com>
2025-05-21 10:49:42 +0300

    Delete app/sss directory

--- app/sss/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SSS() {
-  return (
-    <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold">Sıkça Sorulan Sorular (S.S.S)</h1>
-      <p>Müşterilerimizden gelen sık sorular ve yanıtları bu sayfada yer almaktadır.</p>
-    </div>
-  );
-}
