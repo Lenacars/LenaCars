d7465c82ebfe92be32fd6f27f650f09f14c9873c
Lenacars <info@lenacars.com>
2025-06-12 14:35:09 +0300

    Delete app/admin directory

--- app/admin/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AdminLoginPage() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Admin Login Test</h1>
-      <p>Bu sayfa görünüyorsa, yol doğru çalışıyor demektir.</p>
-    </div>
-  )
-}
-
